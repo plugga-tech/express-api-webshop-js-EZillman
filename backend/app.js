4453aedea50c75a6cc02680d60f2caff194a68d3
EZillman <evelina.zillman@medieinstitutet.se>
2023-03-22 22:45:43 +0100

    connected to database, added a schema

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,9 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv')
+
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -20,5 +23,19 @@ app.use('/', indexRouter);
 app.use('/api/users', usersRouter);
 app.use('/api/orders', ordersRouter);
 app.use('/api/products', productsRouter);
+
+require('dotenv').config();
+
+async function init() {
+    try {
+        const options = { useNewUrlParser: true, useUnifiedTopology: true }
+        await mongoose.connect(process.env.DATABASE_URL, options)
+        console.log('Connected to cloud database');
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+init();
 
 module.exports = app;
