cc7486649deb0773e2d328aeddfae1e63cb35519
EZillman <evelina.zillman@medieinstitutet.se>
2023-03-22 16:30:13 +0100

    routes

--- backend/routes/orders.js
+++ backend/routes/orders.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function (req, res, next) {
-  res.send('Users');
+    res.send('Orders');
 });
 
 module.exports = router;
--- backend/routes/products.js
+++ backend/routes/products.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function (req, res, next) {
-  res.send('Users');
+    res.send('Products');
 });
 
 module.exports = router;
