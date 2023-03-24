55ddc8dabbf192ad2813f7185581adce1cd75ce2
EZillman <evelina.zillman@medieinstitutet.se>
2023-03-24 18:00:37 +0100

    get specific user

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -4,13 +4,45 @@ const userModel = require('../models/user-model');
 
 /* GET users listing. */
 router.get('/', async (req, res) => {
-  const users = await userModel.find({}, 'name email id')
-  res.status(200).json(users)
+
+
+  try {
+    const users = await userModel.find({}, 'name email id');
+
+
+    res.status(200).json(users);
+
+  } catch (err) {
+    console.log(err);
+    res.status(500).send('No users found');
+  }
+
 });
 
+router.post('/', async (req, res) => {
+  const userId = req.body.id;
+
+  try {
+    const user = await userModel.findById(userId);
+
+
+    if (!user) {
+      return res.status(404).send('No user found');
+    }
+
+    res.json(user);
+
+  } catch (err) {
+    console.log(err);
+    res.status(500).send('Server error');
+  }
+
+});
+
+
 router.post('/add', async (req, res) => {
-  const post = await userModel.create(req.body)
-  res.status(201).json(post)
+  const user = await userModel.create(req.body);
+  res.status(201).json(user);
 });
 
 module.exports = router;
