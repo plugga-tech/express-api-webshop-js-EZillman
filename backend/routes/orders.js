58ce5bcd8118ed23c36e3180a973bf8b7b59b0bf
EZillman <evelina.zillman@medieinstitutet.se>
2023-03-26 03:25:16 +0200

    preparing for order endpoints

--- backend/routes/orders.js
+++ backend/routes/orders.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+const orderModel = require('../models/order-model');
 
 /* GET users listing. */
 router.get('/', function (req, res, next) {
