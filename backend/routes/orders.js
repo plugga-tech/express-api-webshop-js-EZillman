a34f0abdaeaf50e3710c5a927aabcbda55ea8f34
EZillman <evelina.zillman@medieinstitutet.se>
2023-04-14 14:39:05 +0200

    update quantity of products in database

--- backend/routes/orders.js
+++ backend/routes/orders.js
@@ -1,8 +1,9 @@
 var express = require('express');
 var router = express.Router();
 const OrderModel = require('../models/order-model');
+const ProductModel = require('../models/product-model');
 
-/* GET users listing. */
+/*
 router.post('/add', async (req, res) => {
     const { user, products } = req.body;
     const order = new OrderModel({ user, products });
@@ -12,6 +13,27 @@ router.post('/add', async (req, res) => {
         res.status(201).json(order);
     } catch (err) {
         res.status(500).send('Could not create order, invalid user id or product id');
+    }
+}); */
+
+router.post('/add', async (req, res) => {
+    try {
+        const { user, products } = req.body;
+
+        for (const item of products) {
+            const product = await ProductModel.findById(item.productId);
+            console.log(product);
+            product.lager -= item.quantity;
+            await product.save();
+        }
+
+        const order = new OrderModel({ user, products });
+        await order.save();
+
+        res.status(201).json(order);
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ error: 'Server error' });
     }
 });
 
